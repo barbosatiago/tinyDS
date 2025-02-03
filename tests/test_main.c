b2a1b7feb090f0ab34c0375ced2e7b9490c6cc6f
barbosatiago <tiago.barbosa@hiontecnologia.com.br>
2025-02-03 15:55:08 -0300

    Primeira compilação com Cmake

---
+++ tests/test_main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//#include "ds_list.h"  // Testando ds_list (ou qualquer outra estrutura)
+
+int main() {
+    printf("Rodando testes...\n");
+
+    // Teste básico (exemplo)
+    printf("Criando lista...\n");
+    // Chame suas funções aqui!
+
+    printf("Todos os testes passaram!\n");
+    return 0;
+}
